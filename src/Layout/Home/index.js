7894e2209dc519040e180f2ceb09c7df18d82769
tusharr42 <tushar.jain@dotsquares.com>
2024-12-16 15:53:19 +0530

    updated

--- src/Layout/Home/index.js
+++ src/Layout/Home/index.js
@@ -1,11 +1,72 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Image from "next/image";
+import ResponsiveAppBar from "../Global/Header/page.js";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Homepageeeeee</h1>
+    
+    <div style={{ position: "relative", width: "1400px", height: "600px" }}>
+      <ResponsiveAppBar />
+      <div style={{height:'83%'}}>
+      <video
+        src="/assets/building1.mp4"
+        autoPlay
+        loop
+        muted
+        style={{
+          width: "100%",
+          height: "500px",
+          objectFit: "cover",
+          zIndex: -1,
+        }}
+      />
+      </div>
+
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          color: "white",
+          fontSize: "48px",
+          fontWeight: "bold",
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        React Flow 2025
+      </div>
+      <div
+        style={{
+          background: "linear-gradient(to right, rgba(0, 0, 255, 0.7), rgba(0, 255, 255, 0.7), rgba(128, 0, 128, 0.7), rgba(238, 130, 238, 0.7), rgba(255, 165, 0, 0.7), rgba(0, 0, 0, 0.7))",
+          color: "white",
+          padding: "20px",
+          textAlign: "center",
+          fontSize: "24px",
+          fontWeight: "bold",
+          width: "100%",
+        }}
+      >
+        Registration is now open now
+        <button
+          style={{
+            backgroundColor: "#CD5C5C" || "transparent",
+            color: "#CCCCFF",
+            padding: "10px 20px",
+            fontSize: "18px",
+            fontWeight: "bold" || "normal",
+            border: "none",
+            borderRadius: "19px",
+            cursor: "pointer",
+            marginLeft: "10px",
+          }}
+        >
+          Register Now
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
