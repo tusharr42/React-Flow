dabe99d54c7448818c46e50e90913ae7c462222e
unknown <darshan.kumar@dotsquares.com>
2024-12-16 17:54:22 +0530

    updated

---
+++ src/app/signup/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignUp from "../../Layout/signup";
+
+
+function Signup() {
+  return (
+    <div>
+        <SignUp/>
+    </div>
+  )
+}
+
+export default Signup;
