d2354a70f526e9dcdde3258691a759d4a9bda4cd
unknown <darshan.kumar@dotsquares.com>
2024-12-18 13:09:57 +0530

    header

--- src/Layout/Global/Header/index.js
+++ src/Layout/Global/Header/index.js
@@ -1,186 +1,40 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import { useRouter } from "next/navigation";
-import AdbIcon from "@mui/icons-material/Adb";
+import React, { useState } from 'react';
+import "./Header.css";
+import Link from 'next/link';
 
-const pages = ["Login"];
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const router = useRouter();
-
-  const handleRedirect = () => {
-    router.push("/login");
-  };
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <AppBar
-      position="static"
-      sx={{
-        backgroundColor: "black",
-        width: "100%",
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              fontSize: "26px",
-              fontWeight: "bold",
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-              cursor: "pointer",
-            }}
-          >
-            React Flow
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "center",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "center",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-                "& .MuiMenuItem-root": {
-                  textAlign: "center",
-                },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: "center", width: "100%" }}>
-                    {page}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "2", md: "2" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          ></Typography>
-          <Box sx={{ flexGrow: 0.1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => {
-                  handleCloseNavMenu();
-                  if (page === "Login") handleRedirect();
-                }}
-                sx={{
-                  my: 2,
-                  color: "white",
-                  display: "block",
-                  marginLeft: "295px",
-                  backgroundColor: page === "Login" ? "#CD5C5C" : "transparent",
-                  fontWeight: page === "Login" ? "bold" : "normal",
-                  borderRadius: page === "Login" ? "17px" : "11px",
-                  textTransform: page === "Login" ? "none" : "uppercase",
-                }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">ReactFlow</div>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+        <ul className={`navbar-menu ${isMenuOpen ? 'menu-open' : ''}`}>
+          <li className="navbar-item">
+            <Link href="/" className="navbar-link">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link href="/about" className="navbar-link">About</Link>
+          </li>
+          <li className="navbar-item">
+            <Link href="/services" className="navbar-link">Services</Link>
+          </li>
+          <li className="navbar-item">
+            <Link href="/contact" className="navbar-link">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
-}
-export default ResponsiveAppBar;
+};
+
+export default Navbar;
