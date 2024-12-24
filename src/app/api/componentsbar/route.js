2983f235215e6ee1c49fcb5de954edb87a76ff16
manishhh2111 <manishkumar2@dotsquares.com>
2024-12-24 11:27:08 +0530

    updated

--- src/app/api/componentsbar/route.js
+++ src/app/api/componentsbar/route.js
@@ -1,24 +1,25 @@
-
-import { NextResponse } from 'next/server';
 import { dbConnection } from '../dbconnection';
 import User from '../Model/user.model';
+import { getSession } from 'next-auth/react'; 
+// Example using next-auth for authentication
 
-// Handler function for the GET method
 export async function GET(req) {
-  try {
-    await dbConnection(); // Ensuring the database is connected
-    console.log('MongoDb connected');
-  } catch (error) {
-    console.error("Database connection error:", error);
-    return NextResponse.json({ message: 'Database connection error', error }, { status: 500 });
+  await dbConnection();
+
+  const session = await getSession({ req });
+  console.log("session", session)
+  if (!session) {
+    return new Response(JSON.stringify({ message: 'Unauthorized' }), { status: 401 });
   }
 
   try {
-    // Fetching unique company names from the database
-    const users = await User.distinct('companyName'); // Using distinct to get unique company names
-    return NextResponse.json(users, { status: 200 });
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return new Response(JSON.stringify({ message: 'User not found' }), { status: 404 });
+    }
+
+    return new Response(JSON.stringify({ companyName: user.companyName, folders: user.folders }), { status: 200 });
   } catch (error) {
-    console.error("Error fetching company names:", error);
-    return NextResponse.json({ message: 'Error fetching company names', error }, { status: 500 });
+    return new Response(JSON.stringify({ message: 'Error fetching company data', error }), { status: 500 });
   }
 }
